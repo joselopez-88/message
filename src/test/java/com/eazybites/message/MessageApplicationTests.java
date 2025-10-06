f3484ad92ca2e79ad5cf916167ecb310797e8b3a
joselopez <alberto.esteva88@gmail.com>
2025-10-05 23:07:20 -0600

    Primer commit - Iniciando proyecto

---
+++ src/test/java/com/eazybites/message/MessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybites.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
