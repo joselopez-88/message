97a66b171c2e4f959b765e9c29ca7a3cb2fcedc7
joselopez <alberto.esteva88@gmail.com>
2025-10-06 17:10:17 -0600

    Implementando Event Driven Service - Part2 - Creando implementacion basica usando Functions para gestion de mensajes

---
+++ src/main/java/com/eazybites/message/functions/MessageFunctions.java
@@ -0,0 +1,32 @@
+package com.eazybites.message.functions;
+
+import java.util.function.Function;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.eazybites.message.dto.AccountsMsgDto;
+
+
+@Configuration
+public class MessageFunctions {
+    private static final Logger logger = LoggerFactory.getLogger(MessageFunctions.class);
+
+    @Bean
+    public Function<AccountsMsgDto, AccountsMsgDto> email(){
+        return  accountsMsgDto -> {
+            logger.info("Sending email with the details: {}", accountsMsgDto);
+            return accountsMsgDto;
+        };
+    }
+
+    @Bean
+    public Function<AccountsMsgDto, Long> sms(){
+        return  accountsMsgDto -> {
+            logger.info("Sending sms with the details: {}", accountsMsgDto);
+            return accountsMsgDto.accountNumber();
+        };
+    }
+}
